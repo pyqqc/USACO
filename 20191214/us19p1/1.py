d1f329372c4026f82b505af0d98a1e86eaaf0ba4
Kainian Xie <1553730177@qq.com>
2019-12-14 08:59:37 +0800

    framework ready

--- 20191214/us19p1/1.py
+++ 20191214/us19p1/1.py
@@ -1,9 +1,25 @@
 """
-ID: mythica2
+ID: xknxkn
 LANG: PYTHON3
-TASK: 
+TASK: gymnastics
 """
-fin = open('','r').readline().strip()
-with open('','w') as fw:
-    fw.write()
+ccn=0
+
+fin = open('gymnastics.in','r')
+kn=fin.readline().strip().split(" ")
+K=int(kn[0])
+N=int(kn[1])
+
+print("K,N",K,N)
+
+trains=[]
+
+for k in range(K):
+    row=fin.readline().strip().split(" ")
+    trains.append(list(map(int,row)))
+print(trains)
+
+
+with open('gymnastics.out','w') as fw:
+    fw.write(str(ccn))
     fw.write('\n')
